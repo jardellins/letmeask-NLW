012ee59126f673d53fba3018b5e890c8d35d5a2b
Jardel Lins <jardel.a7x@gmail.com>
2021-10-07 21:30:01 -0300

    create context and auth firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
+import { useHistory } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss'
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push("/rooms/new");
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -19,15 +35,15 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-        <form>
-          <input type="text" placeholder="Digite o código da sala" />
-          <button type="submit">Entrar na sala</button>
-        </form>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
       </main>
     </div>
